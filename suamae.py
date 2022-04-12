ce2ea5f7a2e971db7a7706da8b836266c0991af2
Gustavo <103332795+JeguinhoXD@users.noreply.github.com>
2022-04-12 17:00:57 -0300

    Update suamae.py

--- suamae.py
+++ suamae.py
@@ -7,10 +7,4 @@ print (f' eu: perdeu tempo otario ')
 outra_coisa = input( ' digite um argumento ')
 
 print(f'eu: ai eu perdi no argumento ')
-~ $ nano suamae
-~ $ nano suamae
-~ $ cat suamae
-qualquer_coisa = input( ' digite qualquer coisa ')
 
-print(f'eu: foda mas ')
-print (f' eu: perdeu tempo otario ')
